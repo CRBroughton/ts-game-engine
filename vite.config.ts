82868c56a1fdc012a89bf27b42f99d4252839ecf
CRBroughton <crbroughton@posteo.uk>
2022-10-21 09:56:28 +0100

    feat(deps): :sparkles: add canvas testing package

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,19 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  test: {
+    setupFiles: ['./vitest.setup.ts'],
+    environment: 'jsdom',
+    deps: {
+      inline: ['vitest-canvas-mock'],
+    },
+    // For this config, check https://github.com/vitest-dev/vitest/issues/740
+    threads: false,
+    environmentOptions: {
+      jsdom: {
+        resources: 'usable',
+      },
+    },
+  },
 })
