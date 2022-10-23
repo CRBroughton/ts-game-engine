53e442d2092fbd28ae544f8bc30e39e718ac5500
CRBroughton <crbroughton@posteo.uk>
2022-10-23 10:37:04 +0100

    refactor(engine): :sparkles: render player to the screen

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,43 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, ref } from 'vue'
+import { gameLoop } from './engine/Engine'
+import type Entity from './engine/Entity'
+import Player from './engine/Player'
+
+let gameObjects: Array<Entity>
+let context: CanvasRenderingContext2D
+const canvasEl = ref<HTMLCanvasElement>()
+
+function createWorld() {
+  gameObjects = [
+    new Player({
+      position: { x: 10, y: 10 },
+      width: 16,
+      height: 16,
+      context,
+      row: (0 * 16),
+      frame: 0,
+      src: 'src/assets/sprite.png',
+    }),
+  ]
+}
+
+function init() {
+  context = canvasEl.value!.getContext('2d')!
+  createWorld()
+  gameLoop(context, canvasEl.value!, gameObjects)
+}
+
+onMounted(() => {
+  init()
+})
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo">
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo">
-    </a>
+  <div class="canvas-wrapper">
+    <canvas id="canvas" ref="canvasEl" width="128" height="128" class="large" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
