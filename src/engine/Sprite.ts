6df968d50dee28d7e4885ad88707861f1cbd2998
CRBroughton <crbroughton@posteo.uk>
2022-10-23 10:35:03 +0100

    feat(sprite): :sparkles: add draw function for sprites

--- src/engine/Sprite.ts
+++ src/engine/Sprite.ts
@@ -1,14 +1,5 @@
 import GameObject from './GameObject'
-import type { GameObjectConfig } from './GameObject'
-
-export interface SpriteType {
-  context: CanvasRenderingContext2D
-  row: number
-  frame: number
-  src: string
-}
-
-type SpriteConfig = SpriteType & GameObjectConfig
+import type { SpriteConfig } from './types'
 
 export default class Sprite extends GameObject {
   context: CanvasRenderingContext2D
@@ -20,12 +11,21 @@ export default class Sprite extends GameObject {
     super(config)
     this.context = config.context
     this.image = new Image()
+    this.image.src = config.src
     this.row = config.row
     this.frame = 0
     this.src = config.src
   }
 
   draw() {
-    this.context.fillRect(this.x, this.y, this.width, this.height)
+    const cropbox = {
+      position: {
+        x: this.width * this.frame,
+        y: this.row,
+      },
+      width: this.width,
+      height: this.height,
+    }
+    this.context.drawImage(this.image, cropbox.position.x, cropbox.position.y, cropbox.width, this.height, this.position.x, this.position.y, this.width, this.height)
   }
 }
