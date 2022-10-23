75fb288c2cdd9cf066b2ef32eb4084ef430b003c
CRBroughton <crbroughton@posteo.uk>
2022-10-23 10:34:12 +0100

    fix(engine): :bug: fix position for GameObjects to be normalised

--- src/engine/GameObject.ts
+++ src/engine/GameObject.ts
@@ -1,18 +1,15 @@
-export interface GameObjectConfig {
-  x: number
-  y: number
-  width: number
-  height: number
-}
+import type { GameObjectConfig, Position } from './types'
 
 export default class GameObject {
-  x: number
-  y: number
   width: number
   height: number
+  position: Position
+
   constructor(config: GameObjectConfig) {
-    this.x = config.x
-    this.y = config.y
+    this.position = {
+      x: config.position.x - config.width / 2,
+      y: config.position.y - config.height / 2,
+    }
     this.width = config.width
     this.height = config.height
   }
