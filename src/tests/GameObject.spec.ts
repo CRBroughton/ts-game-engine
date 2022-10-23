0795c0bf5a95e8e676721e170193b676957cf526
CRBroughton <crbroughton@posteo.uk>
2022-10-23 10:33:32 +0100

    test(engine): :white_check_mark: update Sprite and GameObject positioning tests

--- src/tests/GameObject.spec.ts
+++ src/tests/GameObject.spec.ts
@@ -3,11 +3,12 @@ import GameObject from '../engine/GameObject'
 
 describe('GameObject tests', () => {
   it('sets the configuration for a GameObject', () => {
-    const object = new GameObject({ height: 10, width: 10, x: 5, y: 5 })
+    const object = new GameObject({ height: 16, width: 16, position: { x: 5, y: 5 } })
 
-    expect(object.height).toEqual(10)
-    expect(object.width).toEqual(10)
-    expect(object.x).toEqual(5)
-    expect(object.y).toEqual(5)
+    expect(object.height).toEqual(16)
+    expect(object.width).toEqual(16)
+
+    expect(object.position.x).toEqual(5 - 16 / 2)
+    expect(object.position.y).toEqual(5 - 16 / 2)
   })
 })
--- src/tests/Sprite.spec.ts
+++ src/tests/Sprite.spec.ts
@@ -7,20 +7,19 @@ describe('Sprite tests', () => {
     const context = canvas.getContext('2d')!
 
     const object = new Sprite({
-      width: 10,
-      height: 10,
-      x: 5,
-      y: 5,
+      width: 16,
+      height: 16,
+      position: { x: 5, y: 5 },
       context,
       row: 1,
       frame: 0, // this is set to 0 by default, thus overriden
       src: './test.png',
     })
 
-    expect(object.height).toEqual(10)
-    expect(object.width).toEqual(10)
-    expect(object.x).toEqual(5)
-    expect(object.y).toEqual(5)
+    expect(object.height).toEqual(16)
+    expect(object.width).toEqual(16)
+    expect(object.position.x).toEqual(5 - 16 / 2)
+    expect(object.position.y).toEqual(5 - 16 / 2)
 
     expect(object.context).toBeTruthy()
     expect(object.context.canvas).toBeTruthy()
